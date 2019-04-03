f4b4c911054ee5b4f6ed5435ba19f45b27abdc28
Xiaoji Chen <xiaoji@uber.com>
2019-04-03 12:09:42 -0700

    mesh layer example

--- examples/website/mesh-ply/app.js
+++ examples/website/mesh-ply/app.js
@@ -1,21 +1,19 @@
 import React, {PureComponent} from 'react';
 import {render} from 'react-dom';
-import DeckGL, {COORDINATE_SYSTEM, PointCloudLayer, OrbitView, LinearInterpolator} from 'deck.gl';
+import DeckGL, {COORDINATE_SYSTEM, SimpleMeshLayer, OrbitView, LinearInterpolator} from 'deck.gl';
 
-import loadPLY from './utils/ply-loader';
+import {PLYLoader} from '@loaders.gl/ply';
+import {loadFile} from '@loaders.gl/core';
 
 const DATA_URL =
   'https://raw.githubusercontent.com/uber-common/deck.gl-data/master/examples/point-cloud-ply/lucy100k.ply';
 
 const INITIAL_VIEW_STATE = {
-  lookAt: [0, 0, 0],
-  distance: OrbitView.getDistance({boundingBox: [1, 1, 1], fov: 30}),
+  target: [0, 0, 0],
   rotationX: 0,
   rotationOrbit: 0,
   orbitAxis: 'Y',
   fov: 30,
-  minDistance: 1.5,
-  maxDistance: 10,
   zoom: 1
 };
 
@@ -27,28 +25,13 @@ class Example extends PureComponent {
 
     this.state = {
       viewState: INITIAL_VIEW_STATE,
-      points: []
+      data: [[0, 0, 0]],
+      mesh: loadFile(DATA_URL, PLYLoader)
     };
 
     this._onLoad = this._onLoad.bind(this);
     this._onViewStateChange = this._onViewStateChange.bind(this);
     this._rotateCamera = this._rotateCamera.bind(this);
-
-    this._loadData();
-  }
-
-  _loadData() {
-    loadPLY(DATA_URL).then(({vertex}) => {
-      const points = [];
-      vertex.x.forEach((_, i) => {
-        points.push({
-          color: [(0.5 - vertex.x[i]) * 255, (vertex.y[i] + 0.5) * 255, 255, 255],
-          normal: [vertex.nx[i], vertex.ny[i], vertex.nz[i]],
-          position: [vertex.x[i], vertex.y[i], vertex.z[i]]
-        });
-      });
-      this.setState({points});
-    });
   }
 
   _onViewStateChange({viewState}) {
@@ -73,16 +56,15 @@ class Example extends PureComponent {
   }
 
   _renderLayers() {
-    const {points} = this.state;
+    const {data, mesh} = this.state;
 
     return [
-      new PointCloudLayer({
-        id: 'point-cloud-layer',
-        data: points,
+      new SimpleMeshLayer({
+        id: 'lucy',
+        data,
+        mesh,
         coordinateSystem: COORDINATE_SYSTEM.IDENTITY,
-        getPosition: d => d.position,
-        getNormal: d => d.normal,
-        radiusPixels: 1
+        getPosition: d => d
       })
     ];
   }
